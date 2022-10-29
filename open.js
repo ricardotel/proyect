dd66ef6e0b9153921e790ca93ef98f9d20eefbd4
ricardotel <116834041+ricardotel@users.noreply.github.com>
2022-10-29 13:52:07 -0500

    Add files via upload

---
+++ open.js
@@ -0,0 +1,15 @@
+
+var ricardo ={
+    nombre  : "ricardo",
+    edad : 16,
+    "\u00Bferes-desarrollador?" : true,
+    Date : new Date(2006,08,03),
+    libro: {
+        titulo:"robinson-crusoe",
+        autor:"daniel-defoe",
+        fecha: new Date(1719, 03 ,25)
+    } 
+    
+    
+}
+console.log(ricardo)
